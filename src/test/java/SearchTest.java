8531703f26556a8a7269d6faf1ff1874ba475cab
Diana Tusa <diana_tusha@yahoo.com>
2019-12-04 15:54:07 +0200

    push

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,45 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchTest {
+    private WebDriver driver;
+
+    @Before
+    public void initializeDriver() {
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void validSearchTest() {
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("Search entire")).sendKeys("jewelry");
+        WebElement searchButton = driver.findElement(By.cssSelector(".search-button"));
+        searchButton.click();
+
+        WebElement searchResultMessageElement = driver.findElement(By.cssSelector("search result"));
+        Assert.assertEquals(searchResultMessageElement.getText(), "SEARCH RESULTS FOR 'JEWELRY'");
+    }
+
+
+        @After
+                public void closeDriver(){
+            driver.quit();
+        }
+
+
+
+
+
+
+    }
+
+
